4c41afed362ef93641828b96f0a5211965a4ebef
jam0824 <session_fixation@hotmail.co.jp>
2026-03-03 07:07:36 +0900

    スキルリスト追加＆画面倍率変更

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,7 @@ export const GRID_WIDTH = 96;
 export const GRID_HEIGHT = 96;
 
 export const INITIAL_SEED = "20260208";
-export const GAME_VIEW_SCALE = 1.5;
+export const GAME_VIEW_SCALE = 2.0;
 
 export const ROOM_COUNT_MIN = 9;
 export const ROOM_COUNT_MAX = 12;
